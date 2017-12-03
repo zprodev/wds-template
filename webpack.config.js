81ff1227649d4c6ef559d172d7d6cdff7df30088
zprodev <zproject.develop@gmail.com>
2017-12-03 16:47:40 +0900

    Changed to support cross platform

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: path.resolve('node_modules'),
+        exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
           presets: [
